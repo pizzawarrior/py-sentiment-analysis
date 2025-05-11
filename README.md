12a525176a2d29663a054e3b171cbe7aa7804dec
pizzawarrior <i.norstad@gmail.com>
2025-05-11 13:46:13 -0700

    file cleanup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #### Sentiment Analysis of E-Commerce Customer Review Data
+### Currently under construction
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
+# Entry point, orchestrate data processing & model execution
+
 def main():
+    # execute cleaning, training, and evaluation of models
     pass
 
 if __name__ == "__main__":
